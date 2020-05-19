a3bd7dfda1bcdf0fedca4a7b5545e7faa4995e50
Myron Apostolakis <myron.apostolakis@sunlight.io>
2020-05-19 08:36:33 +0300

    remove progress bar for react dependency

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,11 @@
 import React from "react";
 import "../css/css.styl";
-// import { ProgressBar } from "../../node_modules/progress_bar_for_react/publish/app.bundle";
-import ProgressBar from "progress_bar_for_react";
 
-const App = (name) => {
-  console.log("ProgressBar", ProgressBar);
-
+const App = () => {
   return (
     <div id="wrapper">
       Hello {name}
-      <div id="container">
-        <ProgressBar
-          percentage={80}
-          tooltip="demo text"
-          fillerExtraStyles={{
-            backgroundColor: "red",
-          }}
-        />
-      </div>
+      <div id="container">I am a react component</div>
     </div>
   );
 };
